cec953eec67078b22d28c92f5e167cc06c24714e
alexfeeney2020@gmail.com <123467468+GhostedCode@users.noreply.github.com>
2023-10-22 17:18:11 +1030

    Updating PyBank main with imports

---
+++ PyBank/main.py
@@ -0,0 +1,19 @@
+#
+#Author: @Alex Feeney
+#
+
+
+
+
+
+#output
+print("""
+Financial Analysis
+
+---------------------------------------------------
+""")
+
+print('Total Months: \n')
+print('Average Change: \n')
+print('Greatest Increase in Profits: \n')
+print('Greatest Decrease in Profits: \n')
